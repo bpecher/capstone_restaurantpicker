55760b65dc0234a839c7d081c1e67270ac547b1d
Joseph Green <dudeiera@gmail.com>
2023-05-08 23:33:28 -0500

    [Finishes #185038215] [Finishes #185133118] Added code that displays the price for each restaurant, and added code to show the hot_and_new restaurants via the Yelp API. For iteration release v0.3.0

--- WpfApp1/WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/WpfApp1/MainWindow.xaml.cs
@@ -139,14 +139,18 @@ namespace WpfApp1
                     // Format the phone number with hyphens
                     string formattedPhone = $"{digitsOnly.Substring(0, 1)}-{digitsOnly.Substring(1, 3)}-{digitsOnly.Substring(4, 3)}-{digitsOnly.Substring(7)}";
 
+                    // Get the price of the business (or set it to "unavailable" if the field doesn't exist
+                    string? price = business.ContainsKey("price") ? (string)business["price"] : "unavailable";
+
                     // Get the rating of the business
                     double rating = (double)business["rating"];
 
                     // Get the number of reviews of the business
                     int reviewCount = (int)business["review_count"];
 
-                    // Add the name, address, phone number, ratings, and review counts of the restaurant to the array
-                    restaurants[i] = $"Restaurant Name: {name}\nAddress: {address}\nPhone Number: {formattedPhone}\nRating: ({rating} stars)\nNumber of Reviews: ({reviewCount})\n";
+                    // Add the name, address, phone number, price, ratings, and review counts of the restaurant to the array
+                    restaurants[i] = $"Restaurant Name: {name} {(price == "unavailable" ? "\nPrice: Unavailable" : "\nPrice: " + price)}" + $"\nAddress: " +
+                        $"{address}\nPhone Number: {formattedPhone}\nRating: ({rating} stars)\nNumber of Reviews: ({reviewCount})\n";
                 }
 
                 // Return the array of restaurants
@@ -159,6 +163,85 @@ namespace WpfApp1
             MessageBox.Show("Welcome to our restaurant recommendation app! \n\nTo get started, choose the number of restaurants from the drop-down and click the 'Search Restaurants' button to initiate a location search.\n\nWe'll show you a list of the top-rated restaurants in your area. The results display based on an adjusted rating value that takes into account the number of ratings.\n\nIf you're feeling indecisive on where to eat, click the 'Random Restaurant' button and we'll randomly select a restaurant from the list for you!\n\nEnjoy your meal!");
         }
 
+        private async void HotAndNewButton_Click(object sender, RoutedEventArgs e)
+        {
+            // Get location of user
+            string location = await GetLocation();
+            LocationTextBlock.Text = location;
+
+            // Get list of hot_and_new restaurants
+            string[] hotAndNewRestaurants = await getHotAndNewRestaurants(location);
+
+            // Clear existing list of restaurants that are shown in the ResultsListBox
+            ResultsListBox.Items.Clear();
+
+            // Add new list of restaurants to ResultsListBox
+            foreach (string restaurants in hotAndNewRestaurants)
+            {
+                ResultsListBox.Items.Add(restaurants);
+            }
+
+            // Show a message box with the total number of results
+            MessageBox.Show($"These are the {hotAndNewRestaurants.Length} newest restaurants that have been added to Yelp in your location.");
+        }
+
+        private async Task<string[]> getHotAndNewRestaurants(string location)
+        {
+            using (var client3 = new HttpClient())
+            {
+                // Add authorization header to the client
+                client3.DefaultRequestHeaders.Add("Authorization", "Bearer 0B0nln3vXhrnfy2wELDtoYSEc1q8bIbo_bKfrgeqcfgf88YMpcS2ge9T9oK0sPRoNH38tlX0AEcxnJzFLguGcqQSJWkUxsP179-KilrCatxgjKxSJXoHSa9XClczZHYx");
+
+                // Get the selected value of the NumRestaurantsComboBox as a ComboBoxItem
+                ComboBoxItem selectedItem = NumRestaurantsComboBox.SelectedItem as ComboBoxItem;
+
+                // Extract the integer value from the Content property of the selected ComboBoxItem
+                int numRestaurants = Int32.Parse(selectedItem.Content.ToString());
+
+                // Use hot_and_new to filter search results to only get hot_and_new restaurants
+                string response = await client3.GetStringAsync($"https://api.yelp.com/v3/businesses/search?term=restaurants&location={location}&limit={numRestaurants}&hot_and_new=true");
+
+                // Put response into a JObject
+                JObject json = JObject.Parse(response);
+
+                // Get "businesses" JArray from the response
+                JArray businesses = (JArray)json["businesses"];
+
+                // If no results, return empty array (or something else if we want that)
+                if (businesses.Count == 0)
+                {
+                    return new string[0];
+                }
+
+                // Create an array to store names/addresses/ratings/review counts of restaurants
+                string[] hotAndNewRestaurants = new string[businesses.Count];
+
+                // Loop through each business in "businesses" JArray
+                for (int i = 0; i < businesses.Count; i++)
+                {
+
+                    // Get the current business as a JObject
+                    JObject business = (JObject)businesses[i];
+
+                    // Get the name of the business
+                    string name = (string)business["name"];
+
+                    // Get the address of the business
+                    string address = string.Join(", ", business["location"]["address1"] + " " + business["location"]["city"],
+                        business["location"]["state"] + " " + business["location"]["zip_code"]);
+
+                    // Get the rating of the business
+                    double rating = (double)business["rating"];
+
+                    // Get review count of the business
+                    int reviewCount = (int)business["review_count"];
+
+                    // Add restaurant to array of restaurants
+                    hotAndNewRestaurants[i] = $"{name}\n{address}\nRating: {rating} stars\n({reviewCount} reviews)\n";
+                }
+                return hotAndNewRestaurants;
+            }
+        }
     }
 }
 
